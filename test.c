9e397f2f672e25d57df480e046557f5dd7b65789
jona512 <38784748+jona512@users.noreply.github.com>
2019-06-17 06:17:05 +0200

    Update and rename t01.c to test.c

--- test.c
+++ test.c
@@ -1,12 +1,7 @@
 #include <math.h>
 #include "sgl.h"
 
-// struct coord {
-//   int x;
-//   int y;
-// };
-
-#define MXSTR 5000
+#define MXSTR 500
 
 int stroke[MXSTR * 2];
 int sc = 0;
@@ -17,18 +12,13 @@ void render() {
 
   float s = (sin(sgl_get_global_time_var() * 0.03) + 1) / 2;
   sgl_set_color(0.1, 0.6, 0.03);
-  // sgl_draw_circle(1000, 500, s * 400.0 + 70);
 
   for (int i = 0; i < sc / 2; i++) {
     int x = stroke[i * 2];
     int y = stroke[i * 2 + 1];
-    // if (!(x == 0 && y == 0)) {
     // sgl_draw_circle(x, y, s * 100.0 + 3.0);
     sgl_draw_rectangle(x, y, s * 100.0 + 3.0, s * 100.0 + 3.0);
-    // }
   }
- 
-  // sgl_draw_line(sc/2, &stroke);
 
   sgl_update_display();
 }
@@ -40,18 +30,12 @@ float mod(float v, float m) {
 int c = 0;
 
 void left_click(int mouse_x, int mouse_y) {
-  // printf("left click at %i %i\n", mouse_x, mouse_y);
-  // c++;
-  // if (mod(c, 5) == 0) {
-  // stroke[sc].x = mouse_x;
-  // stroke[sc].y = mouse_y;
   stroke[sc] = mouse_x;
   stroke[sc + 1] = mouse_y;
   sc += 2;
   if (sc > MXSTR * 2) {
     sc = 0;
   }
-  // }
 }
 
 int main(int argc, char** argv) {
